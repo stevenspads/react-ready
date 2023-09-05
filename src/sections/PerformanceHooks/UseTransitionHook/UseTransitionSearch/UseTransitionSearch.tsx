020adb652ff54ff5dd85fe26ecf91d59de996cfe
Steven <stevenspadotto@gmail.com>
2023-09-04 22:54:27 -0400

    useTransitionSearch

--- src/sections/PerformanceHooks/UseTransitionHook/UseTransitionSearch/UseTransitionSearch.tsx
+++ src/sections/PerformanceHooks/UseTransitionHook/UseTransitionSearch/UseTransitionSearch.tsx
@@ -1,29 +1,65 @@
-import { useState, useTransition, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useMemo, useTransition } from 'react';
 
+// SearchNumbers without useTransition()
+/*
 const SearchNumbers = ({ numbers }: { numbers: number[] }) => {
-  const [search, setSearch] = useState('');
-  const [results, setResults] = useState(numbers);
-  const [isPending, startTransition] = useTransition();
+  const [text, setText] = useState("");
+  const [query, setQuery] = useState("");
 
   const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-    setSearch(value);
-    startTransition(() => {
-      setResults(numbers.filter((n) => n.toString().includes(value)));
-    });
+    setText(value);
+    setQuery(value);
   };
+
+  const numbersList = useMemo(() => (
+    numbers.map((i, index) => (
+      query
+        ? i.toString().startsWith(query)
+        && <p key={index}>{i}</p>
+        : <p key={index}>{i}</p>
+    ))
+  ), [query]);
 
   return (
     <>
-      <p>{numbers.length !== results.length ? `${results.length} matches` : null}</p>
+      <input onChange={handleChange} value={text} type="text" />
 
-      <input onChange={handleChange} value={search} type="text" />
+      {numbersList}
+    </>
+  );
+};
+*/
 
-      {isPending ? <div>Loading...</div> : results.map((result) => <p key={result}>{result}</p>)}
+const SearchNumbers = ({ numbers }: { numbers: number[] }) => {
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('');
+  const [isPending, startTransition] = useTransition();
+
+  const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setText(value);
+    startTransition(() => {
+      setQuery(value);
+    });
+  };
+
+  const numbersList = useMemo(
+    () =>
+      numbers.map((i, index) =>
+        query ? i.toString().startsWith(query) && <p key={index}>{i}</p> : <p key={index}>{i}</p>,
+      ),
+    [query],
+  );
+
+  return (
+    <>
+      <input onChange={handleChange} value={text} type="text" />
+
+      {isPending ? <div>Loading...</div> : numbersList}
     </>
   );
 };
 
-const numbers = Array.from(Array(100000).keys());
+const numbers = [...new Array(50000).keys()];
 
 const UseTransitionSearch = () => {
   return (
